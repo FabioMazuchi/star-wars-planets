58c2c7fa934dd11498ec3bf218d8457d9cb5b5f2
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-02-28 09:47:35 -0300

    add req 5

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,11 +1,21 @@
-import React, { useContext, useState } from 'react';
-import Context from '../context/Context';
+import React, { useContext, useState, useEffect } from "react";
+import Context from "../context/Context";
+
+const FILTERS = [
+  'population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water',
+];
 
 function Formulario() {
-  const [column, setColumn] = useState('population');
-  const [comparsion, setComparsion] = useState('maior que');
-  const [value, setValue] = useState('0');
-  const { filterByNumericValues, setFilterByNumericValues } = useContext(Context);
+  const [column, setColumn] = useState("population");
+  const [comparsion, setComparsion] = useState("maior que");
+  const [value, setValue] = useState("0");
+  const [filters, setFilters] = useState(FILTERS);
+  const { filterByNumericValues, setFilterByNumericValues } =
+    useContext(Context);
 
   function addObj() {
     const obj = {
@@ -16,22 +26,37 @@ function Formulario() {
     setFilterByNumericValues([...filterByNumericValues, obj]);
   }
 
+  useEffect(() => {
+    filterByNumericValues.forEach(element => {
+      const valor = Object.values(element)[0];
+      const filtros = filters.slice();
+      const indice = filtros.indexOf(valor);
+      console.log(indice);
+      console.log('valor: ' + valor);
+      if (indice !== -1) {
+        filtros.splice(indice, 1);
+      }
+      console.log(filtros);
+      setFilters(filtros);
+      setColumn(filtros[0]);
+    });
+ 
+  }, [filterByNumericValues]);
+
   return (
     <form>
       <select
-        value={ column }
-        onChange={ ({ target }) => setColumn(target.value) }
+        value={column}
+        onChange={({ target }) => setColumn(target.value)}
         data-testid="column-filter"
       >
-        <option>population</option>
-        <option>orbital_period</option>
-        <option>diameter</option>
-        <option>rotation_period</option>
-        <option>surface_water</option>
+        {filters.map((filter => (
+          <option key={filter}>{filter}</option>
+        )))}
       </select>
       <select
-        value={ comparsion }
-        onChange={ ({ target }) => setComparsion(target.value) }
+        value={comparsion}
+        onChange={({ target }) => setComparsion(target.value)}
         data-testid="comparison-filter"
       >
         <option>maior que</option>
@@ -39,12 +64,12 @@ function Formulario() {
         <option>igual a</option>
       </select>
       <input
-        value={ value }
-        onChange={ ({ target }) => setValue(target.value) }
+        value={value}
+        onChange={({ target }) => setValue(target.value)}
         data-testid="value-filter"
         type="number"
       />
-      <button onClick={ addObj } data-testid="button-filter" type="button">
+      <button onClick={addObj} data-testid="button-filter" type="button">
         Filtrar
       </button>
     </form>
