ec8800c0a3133feec19c91984c4b48f81c2384de
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-08-31 15:45:50 -0300

    Updates

---
+++ precache-manifest.e23143e72b542a0aeffec1d46ed214f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765dd58096ca551b0af2af001488da19",
+    "url": "/star-wars-planets/index.html"
+  },
+  {
+    "revision": "c0983ca2b52cc0ea3b0f",
+    "url": "/star-wars-planets/static/css/main.947dc9d1.chunk.css"
+  },
+  {
+    "revision": "f811f2e241fac56e14e7",
+    "url": "/star-wars-planets/static/js/2.b35840fa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-planets/static/js/2.b35840fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0983ca2b52cc0ea3b0f",
+    "url": "/star-wars-planets/static/js/main.d9c15d55.chunk.js"
+  },
+  {
+    "revision": "6b480a4a8b5688e80087",
+    "url": "/star-wars-planets/static/js/runtime-main.6f08aa3c.js"
+  },
+  {
+    "revision": "ccadf54377a8d01549dec31bcf771dde",
+    "url": "/star-wars-planets/static/media/github.ccadf543.png"
+  },
+  {
+    "revision": "5bb3ed2baaad92f1a4293abcd4b5e1eb",
+    "url": "/star-wars-planets/static/media/linkedin.5bb3ed2b.png"
+  },
+  {
+    "revision": "e537f6ba1a83eb2d6a178d3d060095be",
+    "url": "/star-wars-planets/static/media/logo.e537f6ba.png"
+  }
+]);
